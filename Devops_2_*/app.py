bd1c8c83c4f9811ed8887fc3fbdd1008e6e5a55c
matveySav <144665338+matveySav@users.noreply.github.com>
2024-09-30 02:15:51 +0300

    Update app.py

--- Devops_2_*/app.py
+++ Devops_2_*/app.py
@@ -20,4 +20,4 @@ def get_hit_count():
 @app.route('/')
 def hello():
     count = get_hit_count()
-    return 'Hello World111111111111! I have been seen {} times.\n'.format(count)
+    return 'Hello World! I have been seen {} times.\n'.format(count)
